48af4477100b2b9d4c3a4da41f97c15341460ace
jIVOYz <ilaprohorov51@tutamail.com>
2024-08-10 08:11:44 +0300

    added some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,7 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', 'N', 'Nzz')
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
