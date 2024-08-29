7eaaafae13e173f30af3bed1e8e6f4d0e6477d84
jIVOYz <ilaprohorov51@gmail.com>
2024-08-29 20:07:17 +0300

    LSP config changes

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -2,13 +2,13 @@ return {
   {
     "Mofiqul/dracula.nvim",
     config = function()
-      -- vim.cmd [[colorscheme dracula]]
+      vim.cmd [[colorscheme dracula]]
     end
   },
   {
     "catppuccin/nvim", name = "catppuccin", priority = 1000,
     config = function()
-      vim.cmd [[colorscheme catppuccin-latte]]
+      -- vim.cmd [[colorscheme catppuccin-latte]]
     end
   }
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,73 +1,71 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-    config = function()
-      ensure_installed = { "tsserver", "lua_ls", "rust_analyzer" }
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp"
-    },
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require('lspconfig')
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+		config = function()
+			ensure_installed = { "tsserver", "html", "cssls", "emmet_language_server", "lua_ls", "rust_analyzer" }
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+      local mason_registry = require("mason-registry")
 
-      -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-      local servers = { "tsserver", "lua_ls", "rust_analyzer", "emmet_language_server", "cssls", "html" }
-      for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup({
-          -- on_attach = my_custom_on_attach,
-          capabilities = capabilities,
-        })
-      end
+			-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+			local servers =
+				{ "tsserver", "lua_ls", "rust_analyzer", "emmet_language_server", "cssls", "html", "clangd", "volar" }
+			for _, lsp in ipairs(servers) do
+				lspconfig[lsp].setup({
+					-- on_attach = my_custom_on_attach,
+					capabilities = capabilities,
+				})
+			end
 
       lspconfig.volar.setup {
-        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
         init_options = {
-          vue = {
-            hybridMode = false,
+          typescript = {
+            tsdk = '/usr/lib/node_modules/typescript/lib',
           },
         },
       }
 
+			-- Add border to pop up windows
+			require("lspconfig.ui.windows").default_options.border = "single"
 
-      -- Adds border to pop up windows
-      require('lspconfig.ui.windows').default_options.border = 'single'
+			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+				border = "single",
+			})
+			vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+				border = "single",
+			})
+			vim.diagnostic.config({
+				float = { border = "single" },
+			})
 
-      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-        vim.lsp.handlers.hover, {
-          border = 'single'
-        }
-      )
-      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-        vim.lsp.handlers.signature_help, {
-          border = 'single'
-        }
-      )
-      vim.diagnostic.config {
-        float = { border = 'single' }
-      }
-
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-      vim.keymap.set("n", "gr", vim.lsp.buf.rename, {})
-      vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>gi", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, {})
-    end,
-  },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>gi", function()
+				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+			end, {})
+		end,
+	},
 }
