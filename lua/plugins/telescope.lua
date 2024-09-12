df4abb74f87a8dc9ea520f400c71065b14555be6
jIVOYz <ilaprohorov51@gmail.com>
2024-09-12 11:06:13 +0300

    feat: make telescope ignore node_modules

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,11 +2,16 @@ return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
-
   config = function()
-  local builtin = require('telescope.builtin')
+    require('telescope').setup({
+      defaults = {
+        file_ignore_patterns = { "node_modules" }
+      }
+    })
+
+    local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>;f', builtin.find_files, {})
-	  vim.keymap.set('n', '<leader>;g', builtin.live_grep, {})
-	  vim.keymap.set('n', '<leader>;b', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>;g', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>;b', builtin.buffers, {})
   end
 }
