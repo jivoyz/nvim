25837fe6bc418a528c1e89468ec4d023a11e4fd0
jIVOYz <ilaprohorov51@gmail.com>
2024-09-12 11:29:20 +0300

    feat: add mini.surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,4 +1,5 @@
 return {
 	{ 'echasnovski/mini.animate', version = '*', config = function() require('mini.animate').setup() end },
 	{ 'echasnovski/mini.pairs', version = '*', config = function() require('mini.pairs').setup() end },
+	{ 'echasnovski/mini.surround', version = '*', config = function() require('mini.surround').setup() end },
 }
