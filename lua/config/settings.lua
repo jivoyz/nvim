420a64776dcfd78237e8db2c26e7f071c24fb896
jIVOYz <ilaprohorov51@gmail.com>
2024-09-28 09:38:57 +0300

    nothing much

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,9 +1,11 @@
 -- vim.cmd('set guicursor=n-v-c-i:block')
-vim.cmd('set expandtab')
-vim.cmd('set tabstop=2')
-vim.cmd('set softtabstop=2')
-vim.cmd('set shiftwidth=2')
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
 
 -- relative line numbers
-vim.cmd('set number relativenumber')
+vim.cmd("set number relativenumber")
+
 vim.opt.termguicolors = true
+vim.opt.conceallevel = 1
--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,26 +1,27 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-    conform.setup({
-      formatters_by_ft = {
-        javascript = { "prettierd" },
-        typescript = { "prettierd" },
-        javascriptreact = { "prettierd" },
-        typescriptreact = { "prettierd" },
-        html = { "prettierd" },
-        css = { "prettierd" },
-        json = { "prettierd" },
-        markdown = { "prettierd" },
-        lua = { "stylua" },
-        rust = { "rustfmt" }
-      }
-    })
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				html = { "prettierd" },
+				css = { "prettierd" },
+				json = { "prettierd" },
+				markdown = { "prettierd" },
+				lua = { "stylua" },
+				rust = { "rustfmt" },
+			},
+			log_level = vim.log.levels.DEBUG,
+		})
 
-    vim.keymap.set({ "n", "v" }, "<leader>gf", function()
-      conform.format({ lsp_fallback = true })
-    end)
-  end
+		vim.keymap.set({ "n", "v" }, "<leader>gf", function()
+			conform.format({ lsp_fallback = true })
+		end)
+	end,
 }
