e56b1c828309a4a801f52d74fb1ca14b756f5896
jIVOYz <prokhorov_ilya@proton.me>
2025-02-07 22:28:13 +0200

    add lsp refernce keybind in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -13,5 +13,6 @@ return {
     vim.keymap.set('n', '<leader>;f', builtin.find_files, {})
     vim.keymap.set('n', '<leader>;g', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>;b', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>;r', builtin.lsp_references, {})
   end
 }
