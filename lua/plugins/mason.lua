9d89160dc4d75810bd4853747849132524f2fe22
jIVOYz <prokhorov_ilya@proton.me>
2025-06-21 14:30:14 +0300

    move mason into a different file

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,23 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup({})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"html",
+					"cssls",
+					"rust_analyzer",
+					"lua_ls",
+				},
+			})
+		end,
+	},
+}
