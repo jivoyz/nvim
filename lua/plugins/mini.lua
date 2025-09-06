a5b5acc893f52d8a379e1e5f33b0f4dd12f92c77
jivoyz <prokhorov_ilya@proton.me>
2025-09-06 13:37:38 +0300

    update mini.nvim configuration

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,16 +1,21 @@
 return {
-	{
-		"echasnovski/mini.pairs",
-		version = "*",
-		config = function()
-			require("mini.pairs").setup()
-		end,
-	},
-	{
-		"echasnovski/mini.surround",
-		version = "*",
-		config = function()
-			require("mini.surround").setup()
-		end,
-	},
+	"echasnovski/mini.nvim",
+	version = "*",
+	config = function()
+		require("mini.pairs").setup()
+		require("mini.surround").setup()
+		require("mini.git").setup()
+		require("mini.diff").setup()
+		require("mini.statusline").setup()
+
+		-- Use only HEAD name as summary string
+		local format_summary = function(data)
+			-- Utilize buffer-local table summary
+			local summary = vim.b[data.buf].minigit_summary
+			vim.b[data.buf].minigit_summary_string = summary.head_name or ""
+		end
+
+		local au_opts = { pattern = "MiniGitUpdated", callback = format_summary }
+		vim.api.nvim_create_autocmd("User", au_opts)
+	end,
 }
